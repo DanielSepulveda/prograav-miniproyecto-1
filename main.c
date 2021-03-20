4ec1bf5e8932fb61b832099d18a54f4c69eb938c
jeg99 <a00824494@itesm.com>
2021-03-20 13:50:25 -0600

    percentages matrix is now being poblated, just need to arrange its values according to ingredients

--- main.c
+++ main.c
@@ -24,8 +24,8 @@ char **append(char **oldMatrix, int *size, const char str[1024]) {
     }
   }
   if(!strFound){
-    char **newMatrix = (char **)realloc(oldMatrix, (*size + 1)*sizeof(char *));
-    newMatrix[*size] = (char *)malloc(1024*sizeof(char));
+    char **newMatrix = (char **)realloc(oldMatrix, (*size + 1) * sizeof(char *));
+    newMatrix[*size] = (char *)malloc(1024 * sizeof(char));
     strcpy(newMatrix[*size], str);
     (*size)++;
     return newMatrix;
@@ -34,8 +34,10 @@ char **append(char **oldMatrix, int *size, const char str[1024]) {
 }
 
 void readRecipes(FILE *ptrFile, RecipesMatrix *recipesMatrix) {
-  int percentage;
-  char word[1024], delim[2] = ":", *token;
+  // for file reading 
+  int percentage, currPercentage, currRecipe ;
+  char word[1024], delim[2] = ":", *token, c;
+
   (*recipesMatrix).recipes = (char **)malloc(sizeof(char *));
   (*recipesMatrix).ingredients = (char **)malloc(sizeof(char *));
   (*recipesMatrix).recipesSize = 0;
@@ -50,11 +52,18 @@ void readRecipes(FILE *ptrFile, RecipesMatrix *recipesMatrix) {
       token = strtok(word, delim);
       // if the string "Recipe" is found before ":"...
       if(strcmp(token, "Recipe") == 0) {
-        (*recipesMatrix).recipesSize++;
+        // allocate recipes names with spaces
+        c = getc(ptrFile);
+        while(c != '\n') {
+          strncat(token, &c, 1);
+          c = getc(ptrFile);
+        }
+        (*recipesMatrix).recipes = append((*recipesMatrix).recipes, &(*recipesMatrix).recipesSize, token);
       }
       // if the string "Ingredients" is found before ":"...
       if(strcmp(word, "Ingredients") == 0) {
         percentage = 0;
+        // TODO: add a condition for percentages exceding 100%
         while(percentage < 100) {
           fscanf(ptrFile, "%1023s", word);
           // separate strings using ":" as delimiter
@@ -65,7 +74,40 @@ void readRecipes(FILE *ptrFile, RecipesMatrix *recipesMatrix) {
           // cast token from str to int
           percentage += atoi(token);
         }
-      } 
+      }
+    }
+
+    // jump to BOF to fill percentages matrix
+    
+    rewind(ptrFile);
+
+    // allocate percentages matrix
+    (*recipesMatrix).percentages = (int **)malloc((*recipesMatrix).recipesSize * sizeof(int *));
+    for(int i = 0; i < (*recipesMatrix).recipesSize; i++)
+      (*recipesMatrix).percentages[i] = (int *)malloc((*recipesMatrix).ingredientsSize * sizeof(int));
+    
+    currRecipe = 0;
+    while(!feof(ptrFile)) {
+      fscanf(ptrFile, "%1023s", word);
+      token = strtok(word, delim);
+      if(strcmp(word, "Ingredients") == 0) {
+        percentage = 0;
+        currPercentage = 0;
+        // TODO: add a condition for percentages exceding 100%
+        while(percentage < 100) {
+          fscanf(ptrFile, "%1023s", word);
+          // separate strings using ":" as delimiter
+          token = strtok(word, delim);
+          // obtain the second token from "ingredient name:percentage"
+          token = strtok(NULL, delim);
+          // insert percentage in percentages matrix
+          (*recipesMatrix).percentages[currRecipe][currPercentage] = atoi(token);
+          // cast token from str to int
+          percentage += atoi(token);
+          currPercentage++;
+        }
+        currRecipe++;
+      }
     }
   }
   else {
@@ -79,8 +121,17 @@ int main (void) {
   FILE *ptrFile = fopen("Recipes.txt", "r");
   RecipesMatrix matrix;
   readRecipes(ptrFile, &matrix);
+
+  printf("Recipes:\n");
+  for(int i = 0; i < matrix.recipesSize; i++) printf("%d. %s\n", i + 1, matrix.recipes[i]);
   
+  printf("Ingredients:\n");
   for(int i = 0; i < matrix.ingredientsSize; i++) printf("%d. %s\n", i + 1, matrix.ingredients[i]);
-  printf("\n%d %d\n", matrix.recipesSize, matrix.ingredientsSize);
   
+  printf("Matrix dims: %d %d\n", matrix.recipesSize, matrix.ingredientsSize);
+  for(int i = 0; i < matrix.recipesSize; i++){
+    for (int j = 0; j < matrix.ingredientsSize; j++) printf("%d ", matrix.percentages[i][j]);
+    printf("\n");
+  }
+
 }
