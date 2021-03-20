484367e1d4589fd61f3ff3fbf13754e12881bbff
Elías Garza <jeg99@pop-os.localdomain>
2021-03-20 12:37:34 -0600

    file reading is now a function & there's a matrix struct

--- main.c
+++ main.c
@@ -33,21 +33,14 @@ char **append(char **oldMatrix, int *size, const char str[1024]) {
   return oldMatrix;
 }
 
-int main (void) {
-
-  // for file reading
+void readRecipes(FILE *ptrFile, RecipesMatrix *recipesMatrix) {
   int percentage;
   char word[1024], delim[2] = ":", *token;
+  (*recipesMatrix).recipes = (char **)malloc(sizeof(char *));
+  (*recipesMatrix).ingredients = (char **)malloc(sizeof(char *));
+  (*recipesMatrix).recipesSize = 0;
+  (*recipesMatrix).ingredientsSize = 0;
 
-  char **ingredients = (char **)malloc(sizeof(char *));
-  int ingredientsSize = 0;
-  char **recipes = (char **)malloc(sizeof(char *));
-  int recipesSize = 0;
-
-  int matrixSize = 0;
-
-  FILE *ptrFile = fopen("Recipes.txt", "r");
-  
   if(ptrFile != NULL) {
     printf("File succesfully opened\n");
 
@@ -57,27 +50,21 @@ int main (void) {
       token = strtok(word, delim);
       // if the string "Recipe" is found before ":"...
       if(strcmp(token, "Recipe") == 0) {
-        recipes++;
+        (*recipesMatrix).recipesSize++;
       }
       // if the string "Ingredients" is found before ":"...
       if(strcmp(word, "Ingredients") == 0) {
         percentage = 0;
         while(percentage < 100) {
           fscanf(ptrFile, "%1023s", word);
-#ifdef DEBUG1
-          printf("%s\n", word);          
-#endif // DEBUG
           // separate strings using ":" as delimiter
           token = strtok(word, delim);
-          ingredients = append(ingredients, &ingredientsSize, token);
+          (*recipesMatrix).ingredients = append((*recipesMatrix).ingredients, &((*recipesMatrix).ingredientsSize), token);
           // obtain the second token from "ingredient name:percentage"
           token = strtok(NULL, delim);
           // cast token from str to int
           percentage += atoi(token);
         }
-#ifdef DEBUG0
-        if(percentage == 100) printf("--------\n");
-#endif // DEBUG
       } 
     }
   }
@@ -85,7 +72,15 @@ int main (void) {
     printf("Error while opening file\n");
   }
 
-  for(int i = 0; i < ingredientsSize; i++) printf("%d. %s\n", i + 1, ingredients[i]);
-  printf("\n%d %d\n", recipesSize, ingredientsSize);
+}
+
+int main (void) {
+
+  FILE *ptrFile = fopen("Recipes.txt", "r");
+  RecipesMatrix matrix;
+  readRecipes(ptrFile, &matrix);
+  
+  for(int i = 0; i < matrix.ingredientsSize; i++) printf("%d. %s\n", i + 1, matrix.ingredients[i]);
+  printf("\n%d %d\n", matrix.recipesSize, matrix.ingredientsSize);
   
 }
